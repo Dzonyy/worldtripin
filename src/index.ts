995e409a7cb65fcd5b15a3dc708f3f279042709a
Dzonyy <paszcza.jakub@gmail.com>
2023-04-06 12:51:28 +0200

    added dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+dotenv.config();
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, TypeScript with Express!');
